debcf6cc7f0ca8134d0c47362850fe3a786ff3b9
Mason-Perdue <perduem08@gmail.com>
2025-02-13 21:42:27 -0800

    install.sh instead of config.sh

--- install.sh
+++ install.sh
@@ -101,6 +101,34 @@ ping archlinux.org
 	nmcli device
 	ping archlinux.org
 git clone https://github.com/Mason-Perdue/Linux-Config.git
-cd Linux-Config
-chmod +x config.sh
-./config.sh
+sudo timedatectl set-timezone America/Los_Angeles
+sudo timedatectl set-ntp true
+sudo timedatectl status
+sudo pacman -Syu
+sudo pacman -S vlc ufw cups simple-scan thunar htop xorg-xwayland alacritty fuzzel gnome-keyring grim gvfs hypridle labwc pamixer pulseaudio pulseaudio-bluetooth seahorse slurp sshfs swaybg swaylock waybar wlr-randr base-devel man-pages man-db cifs-utils dosfstools ntfsprogs exfat-utils brightnessctl stow # firefox spotify-launcher
+# https://aur.archlinux.org/packages/google-chrome
+curl -LO https://aur.archlinux.org/cgit/aur.git/snapshot/google-chrome.tar.gz
+tar -xvf google-chrome.tar.gz
+rm google-chrome.tar.gz
+cd google-chrome
+makepkg -sirc
+cd ~/Linux-Config
+rm -r google-chrome
+cd ~/Linux-Config/dotconfig
+stow -t ~/ --restow *
+sudo cp ~/Linux-Config/login /etc/pam.d/login
+chmod +x ~/.config/labwc/scripts/*
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+# sudo ufw allow from 192.168.0.0/24 to any port 753 proto tcp
+# sudo ufw allow CIFS
+sudo ufw enable
+sudo systemctl enable --now ufw
+sudo systemctl status ufw
+sudo ufw status verbose
+sudo systemctl enable --now cups.service
+sudo systemctl enable --now seatd.service
+# sudo systemctl enable --now bluetooth
+sudo gpasswd -a $USER seat
+sudo gpasswd -a $USER video
+sudo reboot now
