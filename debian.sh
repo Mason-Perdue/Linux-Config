40cbeec853a6c98c3457f50baea80ff20176f422
Mason-Perdue <perduem08@gmail.com>
2025-02-20 14:42:49 -0800

    spotify install

--- debian.sh
+++ debian.sh
@@ -68,7 +68,9 @@
 	git clone https://github.com/Mason-Perdue/Linux-Config.git
 	cd Linux-Config/config/
 	stow -t ~/ --restow *
-
+	curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+	sudo apt-get update && sudo apt-get install spotify-client
 
 
 
